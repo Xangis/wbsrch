5648681f167b1a69537d8b75a50364bd2cb5f704
= <=>
2016-03-05 21:03:25 -0800

    Script to run some test queries against the API using CURL.

---
+++ apitest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -X GET https://wbsrch.com/api/domain_link_rank/?domain=youporn.com -H 'Authorization: Token ed740e11d3f0e7383d1c7a05a204d6c5f9c501d6'
+curl -X GET https://wbsrch.com/api/domain_link_rank/?domain=www.tacobell.com -H 'Authorization: Token ed740e11d3f0e7383d1c7a05a204d6c5f9c501d6'
