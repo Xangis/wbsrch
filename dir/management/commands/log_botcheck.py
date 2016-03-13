649535ee62ee5280c0c84c883c08e92c8b92df90
Jason Champion <jchampion@zetacentauri.com>
2016-03-13 10:23:16 -0700

    Add more bot strings, add ability to check all languages at once with -e in log_botcheck.

--- dir/management/commands/log_botcheck.py
+++ dir/management/commands/log_botcheck.py
@@ -3,27 +3,37 @@ from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from optparse import make_option
 from dir.utils import GetSearchLogModelFromLanguage, IsBotAgent
+from dir.models import language_list
 import time
+
+def CheckLogsForBots(lang, options):
+    log_model = GetSearchLogModelFromLanguage(lang)
+    total = log_model.objects.all().count()
+    logs = log_model.objects.filter(is_bot=False, browserstring__isnull=False)
+    print u'{0} total log entries in {1}, {2} need to be checked'.format(total, lang, logs.count())
+    changed = 0
+    checked = 0
+    for log in logs:
+        if IsBotAgent(log.browserstring):
+            log.is_bot = True
+            log.save()
+            changed += 1
+        checked = checked + 1
+        if checked % 100000 == 0:
+            print u'{0} checked.'.format(checked)
+    print u'{0} of {1} log entries in {2} were checked. {3} newly marked as bots. Others were already bots or had no browser string.'.format(checked, total, lang, changed)
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
-        make_option('-l', '--language', default='en', action='store', type='string', dest='language', help='Language to use for pending indexes (default=en).'),
+        make_option('-l', '--language', default='en', action='store', type='string', dest='language', help='Language to use for search logs (default=en).'),
+        make_option('-e', '--everything', default=False, action='store_true', dest='everything', help='Process everything, all languages.'),
     )
 
     def handle(self, *args, **options):
         lang = options['language']
-        log_model = GetSearchLogModelFromLanguage(lang)
-        total = log_model.objects.all().count()
-        print u'{0} total log entries'.format(total)
-        logs = log_model.objects.filter(is_bot=False, browserstring__isnull=False)
-        changed = 0
-        checked = 0
-        for log in logs:
-            if IsBotAgent(log.browserstring):
-                log.is_bot = True
-                log.save()
-                changed += 1
-            checked = checked + 1
-            if checked % 100000 == 0:
-                print u'{0} checked.'.format(checked)
-        print u'{0} of {1} log entries were checked. {2} newly marked as bots. Others were already bots or had no browser string.'.format(checked, total, changed)
+        everything = options['everything']
+        if everything:
+            for lang in language_list:
+                CheckLogsForBots(lang, options)
+        else:
+            CheckLogsForBots(lang, options)
