c7645e02135fcffddf98500d2481a1afd0efa1bc
Jason Champion <jchampion@zetacentauri.com>
2016-03-25 22:30:28 -0700

    Change robots daemon to select 50 at at time to reduce big query frequency.

--- robots_daemon.py
+++ robots_daemon.py
@@ -3,6 +3,6 @@ import time
 
 while True:
     # Get them in batches of twenty, with a short break
-    call(['python', 'manage.py', 'robots_update', '-m', '20', '-s', '0', '-i'])
+    call(['python', 'manage.py', 'robots_update', '-m', '50', '-s', '0', '-i'])
     time.sleep(1)
     
