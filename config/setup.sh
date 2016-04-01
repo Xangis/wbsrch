ac2ac637af8749903fa0bb2a3ea600e5d48bb5ca
root <root@wbsrch.com>
2016-03-31 20:31:52 -0700

    Add pg_hba.conf and copying that to postgres configs during setup.

--- config/setup.sh
+++ config/setup.sh
@@ -4,12 +4,17 @@
 # We are not there yet.
 echo Installing apt packages.
 sudo apt-get install lm-sensors python python-virtualenv virtualenvwrapper libpq-dev python-dev postgresql-9.3-postgis-2.1 git zip unzip bind9 bind9utils openssh-server nginx build-essential usbmount memcached aptitude sysstat pgtune automake libtool gdb uwsgi uwsgi-plugin-python libgeoip-dev
+echo Copying Postgres pg_hba.conf.
+sudo cp pg_hba.conf /etc/postgresql/9.3/main/pg_hba.conf
+sudo chown postgres:postgres /etc/postgresql/9.3/main/pg_hba.conf
 echo Copying NGINX config.
 sudo cp wbsrch.com /etc/nginx/sites-available/wbsrch.com
 echo Linking NGINX config.
 sudo ln -s /etc/nginx/sites-available/wbsrch.com /etc/nginx/sites-enabled/wbsrch.com
 echo Copying NGINX IP block list.
 sudo cp blockips.conf /etc/nginx/blockips.conf
+echo Removing default NGINX config.
+sudo rm /etc/nginx/sites-enabled/default
 echo Testing NGINX config.
 sudo nginx -t
 echo Copying WSGI config.
