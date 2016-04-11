63a2f95aa83ab67d571f09715caf7b7d14d0894d
Jason Champion <jchampion@zetacentauri.com>
2016-04-10 21:24:09 -0700

    Add print parameter to add_new_to_pending management command.

--- dir/management/commands/add_new_to_pending.py
+++ dir/management/commands/add_new_to_pending.py
@@ -14,6 +14,7 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('-l', '--language', default='en', action='store', type='string', dest='language', help='Language to use for pending indexes (default=en).'),
         make_option('-d', '--domains', default=False, action='store_true', dest='domains', help='Process file as a domain list and print the domains that need to be crawled. (default=no).'),
+        make_option('-p', '--print', default=False, action='store_true', dest='print', help='Just print the terms that are not indexed to the screen, do not create pending terms. (default=no).'),
         make_option('-n', '--noindividual', default=False, action='store_true', dest='noindividual', help='Make sure individual words of a phrase are not indexed (default=False)'),
         make_option('-m', '--maxwords', default=5, action='store', type='int', dest='maxwords', help='Max number of terms to index. (default=5)'),
         make_option('-f', '--file', default=None, action='store', type='string', dest='file', help='Load term list from specified file.'),
@@ -24,6 +25,7 @@ class Command(BaseCommand):
         maxwords = options.get('maxwords', 0)
         language = options.get('language', 'en')
         domains = options.get('domains', False)
+        printem = options.get('print', False)
         noindividual = options.get('noindividual', None)
         term_model = GetIndexModelFromLanguage(language)
         print 'Using file {0} with {1} max words and language {2}.'.format(filename, maxwords, language)
@@ -42,8 +44,11 @@ class Command(BaseCommand):
                  else:
                      term = term_model.objects.get(keywords=line)
              except ObjectDoesNotExist:
-                 if domains:
-                     print u'{0}'.format(line)
+                 if domains or printem:
+                     try:
+                         print u'{0}'.format(line)
+                     except:
+                         pass
                  else:
                      AddPendingTerm(line, language, 'add_new_to_pending from file {0}'.format(filename))
                      print u'Added {0}.'.format(line)
@@ -55,8 +60,15 @@ class Command(BaseCommand):
                          try:
                              term = term_model.objects.get(keywords=word)
                          except ObjectDoesNotExist:
-                             AddPendingTerm(word, language, 'add_new_to_pending from file {0}'.format(filename))
-                             print u'Added {0}.'.format(word)
+                             if not printem:
+                                 try:
+                                     AddPendingTerm(word, language, 'add_new_to_pending from file {0}'.format(filename))
+                                 except:
+                                     pass
+                             try:
+                                 print u'{0}.'.format(word)
+                             except:
+                                 pass
                              numadded = numadded + 1
              numdone = numdone + 1
              if numdone >= maxwords:
