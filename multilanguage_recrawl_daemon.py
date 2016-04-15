cf6ee972b3b6e7bdb326c90c9f0c37996e00fab0
Jason Champion <jchampion@zetacentauri.com>
2016-04-15 16:05:33 -0700

    Add missing multilanguage recraw daemon.

---
+++ multilanguage_recrawl_daemon.py
@@ -0,0 +1,16 @@
+from subprocess import call
+import time
+
+import os
+import sys
+sys.path.append('/var/django/wbsrch/')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'zetaweb.settings'
+
+from dir.views import language_list
+
+while True:
+    for x in range(2000, 0, -20):
+        for language in language_list:
+            if language is 'en':
+                continue
+            call(['python', 'manage.py', 'crawl', '-r', '-m', '20', '-s', '5', '-o', str(x), '-l', language])
