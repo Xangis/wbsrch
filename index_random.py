4ed35e8e7d14c0240558c141955861a1e7cebb79
Jason Champion <jchampion@zetacentauri.com>
2016-04-20 21:57:22 -0700

    Increase reindex number for index_random.py.

--- index_random.py
+++ index_random.py
@@ -6,7 +6,7 @@ while True:
     call(['python', 'manage.py', 'index', '-p', '-m', '10', '-s', '0', '-x'])
     # Wait 5 seconds between context switches
     time.sleep(1)
-    call(['python', 'manage.py', 'index', '-r', '-m', '2', '-s', '0', '-o', '50'])
+    call(['python', 'manage.py', 'index', '-r', '-m', '5', '-s', '0', '-o', '50'])
     # Wait 5 seconds between cycles.
     time.sleep(1)
     
