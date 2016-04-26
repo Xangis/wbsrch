a55fb12252bf07676b025849a466e448a38a5160
Jason Champion <jchampion@zetacentauri.com>
2016-04-26 13:53:01 -0700

    Modify reindex daemon to work safely alongside index_random and index_daemon by changing the index offset.

--- reindex_daemon.py
+++ reindex_daemon.py
@@ -2,7 +2,7 @@ from subprocess import call
 import time
 
 while True:
-    call(['python', 'manage.py', 'index', '-r', '-m', '10', '-s', '0'])
+    call(['python', 'manage.py', 'index', '-r', '-m', '10', '-s', '0', '-o', '100'])
     # Wait 5 seconds between cycles.
     time.sleep(1)
     
