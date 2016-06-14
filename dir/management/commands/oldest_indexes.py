b6ad16cc9c6d7431bab013362e34befbe669fbe9
Jason Champion <jchampion@zetacentauri.com>
2016-06-14 10:26:19 -0700

    Add average index age commandline argument to oldest_indexes admin command.

--- dir/management/commands/oldest_indexes.py
+++ dir/management/commands/oldest_indexes.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ObjectDoesNotExist
+from django.utils import timezone
 from zetaweb import settings
 from optparse import make_option
 from dir.models import PendingIndex, IndexTerm, DomainInfo
@@ -16,6 +17,7 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('-d', '--date', default=None, action='store', type='string', dest='date', help='Date to show old index or url counts from (default=do not show).'),
         make_option('-o', '--oldest', default=False, action='store_true', dest='oldest', help='Show oldest indexes (dfault=False).'),
+        make_option('-a', '--averageage', default=False, action='store_true', dest='averageage', help='Show average age, takes a long time (dfault=False).'),
         make_option('-s', '--showempty', default=False, action='store_true', dest='showempty', help='Show empty index counts (use with -d).'),
         make_option('-u', '--urls', default=False, action='store_true', dest='showurls', help='Show oldest urls by language.'),
     )
@@ -34,6 +36,22 @@ class Command(BaseCommand):
         for language in language_list:
             index_model = GetIndexModelFromLanguage(language)
             site_model = GetSiteInfoModelFromLanguage(language)
+            if options['averageage']:
+                items = index_model.objects.all().values('date_indexed')
+                numitems = 0
+                totalage = 0
+                nowtime = timezone.now().date()
+                for item in items:
+                    dateindexed = item['date_indexed'].date()
+                    numitems = numitems + 1
+                    indexage = (nowtime - dateindexed).days
+                    totalage += indexage
+                averageage = totalage / numitems
+                if sdate:
+                    minage = (nowtime - sdate.date()).days
+                    if averageage < minage:
+                        continue
+                msgs3.append(u'Average index age is {0} days for language {1}'.format(averageage, language))
             if options['oldest']:
                 items = index_model.objects.order_by('date_indexed')[0:1]
                 for item in items:
@@ -62,6 +80,8 @@ class Command(BaseCommand):
         for message in msgs1:
             print message
         for message in msgs2:
+            print message
+        for message in msgs3:
             print message
         if sdate:
             print u'{0} old indexes total.'.format(total_items)
