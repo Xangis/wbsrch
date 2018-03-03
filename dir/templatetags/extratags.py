70d75441385e2d02966e1ab29f1281581a84ae41
Jason Champion <Private>
2018-03-03 23:05:42 +0000

    Add a safe language_name tag to prevent exceptions for domains tagged as blocked languages.

--- dir/templatetags/extratags.py
+++ dir/templatetags/extratags.py
@@ -1,7 +1,15 @@
 from django import template
+from django.utils import translation
 
 register = template.Library()
 
 @register.filter()
 def to_int(value):
     return int(value)
+
+@register.filter()
+def language_name_safe(value):
+    try:
+        val = translation.get_language_info(value)['name']
+    except KeyError:
+        return value
