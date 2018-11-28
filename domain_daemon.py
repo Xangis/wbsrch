ccedca82fca3ba692b0dd4bc9ef20fb9c825fd55
Jason Champion <Private>
2018-11-28 05:26:24 +0000

    Run domain daemon in larger batches because startup overhead is fairly high.

--- domain_daemon.py
+++ domain_daemon.py
@@ -3,6 +3,6 @@ import time
 
 while True:
     # Update domains slowly to avoid annoying DNS servers.
-    call(['python', 'manage.py', 'domain_update', '-m', '20', '-s', '30', '-r'])
+    call(['python', 'manage.py', 'domain_update', '-m', '480', '-s', '30', '-r'])
     time.sleep(5)
     
