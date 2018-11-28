6e4e907a17b5233bbd58b59dff16fc10a8524f6d
Jason Champion <Private>
2018-11-28 05:24:04 +0000

    Generate index stats from command line runs in verbose mode now.

--- dir/management/commands/generate_index_stats.py
+++ dir/management/commands/generate_index_stats.py
@@ -3,5 +3,5 @@ from dir.utils import GenerateIndexStats
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        stats = GenerateIndexStats(True)
+        stats = GenerateIndexStats(True, True)
         print 'Done. ' + str(stats.total_urls) + ' URLs crawled, ' + str(stats.total_indexes) + ' terms indexed, ' + str(stats.total_pendingindexes) + ' indexes pending, and ' + str(stats.num_excluded) + ' excluded sites.'
