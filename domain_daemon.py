cf15627378a2c1723f38e6a1640e5854e9c99585
Jason Champion <jchampion@zetacentauri.com>
2018-12-29 16:22:47 -0800

    Change domain daemon to use shorter sleep interval, larger batches.

--- domain_daemon.py
+++ domain_daemon.py
@@ -3,6 +3,6 @@ import time
 
 while True:
     # Update domains slowly to avoid annoying DNS servers.
-    call(['python', 'manage.py', 'domain_update', '-m', '480', '-s', '30', '-r'])
+    call(['python', 'manage.py', 'domain_update', '-m', '1000', '-s', '10', '-r'])
     time.sleep(5)
-    
+
