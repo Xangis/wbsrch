f9b0b75506cfbceebabfea7e720f51d81fef2995
Jason Champion <jchampion@zetacentauri.com>
2019-02-12 23:44:49 +0000

    Update daemon timing.

--- domain_daemon.py
+++ domain_daemon.py
@@ -2,7 +2,9 @@ from subprocess import call
 import time
 
 while True:
-    # Update domains slowly to avoid annoying DNS servers.
-    call(['python', 'manage.py', 'domain_update', '-m', '1000', '-s', '300', '-r'])
+    # Update domains slowly to avoid annoying DNS servers. Do sequential, then random.
+    call(['python', 'manage.py', 'domain_update', '-m', '1000', '-s', '20'])
+    time.sleep(5)
+    call(['python', 'manage.py', 'domain_update', '-m', '1000', '-s', '20', '-r'])
     time.sleep(5)
 
