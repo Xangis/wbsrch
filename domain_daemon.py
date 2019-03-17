d1daf812318ac19a9d4786138d0761b436e56c0d
Jason Champion <jchampion@zetacentauri.com>
2019-03-17 00:11:25 +0000

    Throttle domain queries less.

--- domain_daemon.py
+++ domain_daemon.py
@@ -3,8 +3,8 @@ import time
 
 while True:
     # Update domains slowly to avoid annoying DNS servers. Do sequential, then random.
-    call(['python', 'manage.py', 'domain_update', '-m', '1000', '-s', '20'])
+    call(['python', 'manage.py', 'domain_update', '-m', '1000', '-s', '10'])
     time.sleep(5)
-    call(['python', 'manage.py', 'domain_update', '-m', '1000', '-s', '20', '-r'])
+    call(['python', 'manage.py', 'domain_update', '-m', '1000', '-s', '10', '-r'])
     time.sleep(5)
 
