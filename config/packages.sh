bb0765409f5353ff8ba7d07426d73dc1b7698e67
Jason Champion <jchampion@zetacentauri.com>
2019-03-30 18:44:11 +0000

    Add exim4 to packages because that's necessary for emailing error messages (otherwise errors get swallowed)

--- config/packages.sh
+++ config/packages.sh
@@ -2,4 +2,4 @@ sudo apt-get remove apache2
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install certbot build-essential nginx python2.7-dev postgresql-client-10 postgresql-10 libpq-dev redis-server gettext node-uglify \
- python-virtualenv virtualenvwrapper libgeoip-dev dnsutils libpqxx-dev lm-sensors unzip usbmount uwsgi uwsgi-plugin-python zip
+ python-virtualenv virtualenvwrapper libgeoip-dev dnsutils libpqxx-dev lm-sensors unzip usbmount uwsgi uwsgi-plugin-python zip exim4
