6699ee72b20afc397617ad8cbbf593492f296751
Jason Champion <jason@peopledatalabs.com>
2019-05-03 11:02:24 -0700

    Add wget_domcop script.

---
+++ wget_domcop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This retrieves the top 10 domcop URLs. It is called by a cron job, but
+# can also be used manually.
+mkdir -p ./urls/domcop
+MYDATESTR=$(date +"%Y-%m-%d")
+MYFILENAME="./urls/domcop/top-1m_$MYDATESTR.csv.zip"
+echo "Getting top10milliondomains.csv.zip as $MYFILENAME"
+wget -O $MYFILENAME https://www.domcop.com/files/top/top10milliondomains.csv.zip
+ls -l $MYFILENAME
