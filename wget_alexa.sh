143bbaf6167667cde7b87433c6d1ddb61c4bd38d
Jason Champion <jchampion@zetacentauri.com>
2019-05-10 19:56:58 +0000

    Make wget commands work for cron, fix cron so that it runs commands successfully.

--- wget_alexa.sh
+++ wget_alexa.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This retrieves the top 1 million Alexa URLs. It is called by a cron job, but
 # can also be used manually.
-mkdir -p ./urls/alexa
-MYDATESTR=$(date +"%Y-%m-%d")
+/bin/mkdir -p ./urls/alexa
+MYDATESTR=$(/bin/date +"%Y-%m-%d")
 MYFILENAME="./urls/alexa/top-1m_$MYDATESTR.csv.zip"
 echo "Getting top-1m.csv.zip as $MYFILENAME"
-wget -O $MYFILENAME http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
-ls -l $MYFILENAME
+/usr/bin/wget -nv -O $MYFILENAME http://s3.amazonaws.com/alexa-static/top-1m.csv.zip
+/bin/ls -l $MYFILENAME
--- wget_domcop.sh
+++ wget_domcop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This retrieves the top 10 domcop URLs. It is called by a cron job, but
 # can also be used manually.
-mkdir -p ./urls/domcop
-MYDATESTR=$(date +"%Y-%m-%d")
+/bin/mkdir -p ./urls/domcop
+MYDATESTR=$(/bin/date +"%Y-%m-%d")
 MYFILENAME="./urls/domcop/top10milliondomains_$MYDATESTR.csv.zip"
 echo "Getting top10milliondomains.csv.zip as $MYFILENAME"
-wget -O $MYFILENAME https://www.domcop.com/files/top/top10milliondomains.csv.zip
-ls -l $MYFILENAME
+/usr/bin/wget -nv -O $MYFILENAME https://www.domcop.com/files/top/top10milliondomains.csv.zip
+/bin/ls -l $MYFILENAME
