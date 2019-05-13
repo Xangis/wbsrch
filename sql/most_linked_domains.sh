f1a544179ce00bf9f4aa498d2e13b409d0173170
Jason Champion <jchampion@zetacentauri.com>
2019-05-12 17:52:02 -0700

    Add sql command to generate most linked to domains list.

---
+++ sql/most_linked_domains.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+time psql -c "COPY ( select url, domains_linking_in from dir_domaininfo where domains_linking_in > 0 order by domains_linking_in desc limit 1000000 ) TO STDOUT WITH CSV HEADER " zetaweb > /tmp/most_linked_domains_2019-05.csv
