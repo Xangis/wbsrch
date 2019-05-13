d63e33f9a099708625a38d09f61fe00da305dcdb
Jason Champion <jchampion@zetacentauri.com>
2019-05-13 02:24:18 -0700

    Include period in queries for domain suffix stat generation.

--- dir/management/commands/domain_suffix_stats.py
+++ dir/management/commands/domain_suffix_stats.py
@@ -15,31 +15,32 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         counts = []
         for tld in tld_set:
-            print('Checking .{0}'.format(tld))
-            num_domains = DomainInfo.objects.filter(url__endswith=tld).values('url').count()
-            print('Domains ending in .{0}: {1}'.format(tld, num_domains))
+            tldwithdot = '.{0}'.format(tld)
+            print('Checking {0}'.format(tldwithdot))
+            num_domains = DomainInfo.objects.filter(url__endswith=tldwithdot).values('url').count()
+            print('Domains ending in {0}: {1}'.format(tldwithdot, num_domains))
             num_domains_with_pages = 0
             for lang in language_list:
                 site_model = GetSiteInfoModelFromLanguage(lang)
-                with_pages = site_model.objects.filter(rooturl__endswith=tld).values('rooturl').distinct().count()
+                with_pages = site_model.objects.filter(rooturl__endswith=tldwithdot).values('rooturl').distinct().count()
                 if with_pages > 0:
-                    print('Num domains ending in .{0} with pages in {1} index: {2}'.format(tld, lang, with_pages))
+                    print('Num domains ending in {0} with pages in {1} index: {2}'.format(tldwithdot, lang, with_pages))
                     num_domains_with_pages += with_pages
-            print('Total num domains ending in .{0} with pages in all indexes: {1}'.format(tld, num_domains_with_pages))
-            num_blocked_domains = BlockedSite.objects.filter(url__endswith=tld).values('url').count()
-            print('Num domains ending in .{0} that are blocked: {1}'.format(tld, num_blocked_domains))
+            print('Total num domains ending in {0} with pages in all indexes: {1}'.format(tldwithdot, num_domains_with_pages))
+            num_blocked_domains = BlockedSite.objects.filter(url__endswith=tldwithdot).values('url').count()
+            print('Num domains ending in {0} that are blocked: {1}'.format(tldwithdot, num_blocked_domains))
             if num_domains_with_pages > 0:
                 ratio = (num_blocked_domains * 100.0) / num_domains_with_pages
                 print('Ratio of blocked domains to domains with pages in index: {0}%'.format(ratio))
             else:
                 ratio = 0
-                print('No domains ending in .{0} in index, cannot calculate ratio.'.format(tld))
+                print('No domains ending in {0} in index, cannot calculate ratio.'.format(tldwithdot))
             counts.append((tld, num_domains, num_domains_with_pages, num_blocked_domains, ratio))
             try:
-                suffix = DomainSuffix.objects.get(extension='.{0}'.format(tld))
+                suffix = DomainSuffix.objects.get(extension=tldwithdot)
             except ObjectDoesNotExist:
                 suffix = DomainSuffix()
-                suffix.extension = '.{0}'.format(tld)
+                suffix.extension = tldwithdot
             suffix.num_known = num_domains
             suffix.num_crawled = num_domains_with_pages
             suffix.num_blocked = num_blocked_domains
