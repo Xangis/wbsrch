e32eb4ab7f6a7520c07636f5625cdaf0eb6755ca
Jason Champion <jchampion@zetacentauri.com>
2020-12-16 14:22:30 -0800

    Linter fixes

--- zetaweb/dbrouter.py
+++ zetaweb/dbrouter.py
@@ -1,24 +1,24 @@
 class ModelDatabaseRouter(object):
     """Allows each model to set its own destiny"""
-    
+
     def db_for_read(self, model, **hints):
         # Specify target database with field in_db in model's Meta class
         if hasattr(model._meta, 'in_db'):
-            #print u'Model has database: {0}'.format(model._meta.in_db)
+            # print u'Model has database: {0}'.format(model._meta.in_db)
             return model._meta.in_db
         return 'default'
 
     def db_for_write(self, model, **hints):
         # Specify target database with field in_db in model's Meta class
         if hasattr(model._meta, 'in_db'):
-            #print u'Model has database: {0}'.format(model._meta.in_db)
-            return model._meta.in_db        
+            # print u'Model has database: {0}'.format(model._meta.in_db)
+            return model._meta.in_db
         return 'default'
-    
-    def allow_syncdb(self, db, model):      
+
+    def allow_syncdb(self, db, model):
         # Specify target database with field in_db in model's Meta class
         if hasattr(model._meta, 'in_db'):
-            #print u'Model has database: {0}'.format(model._meta.in_db)
+            # print u'Model has database: {0}'.format(model._meta.in_db)
             if model._meta.in_db == db:
                 return True
             else:
@@ -32,7 +32,7 @@ class ModelDatabaseRouter(object):
 
     def allow_migrate(self, db, model):
         if hasattr(model._meta, 'in_db'):
-            #print u'Model has database: {0}'.format(model._meta.in_db)
+            # print u'Model has database: {0}'.format(model._meta.in_db)
             if model._meta.in_db == db:
                 return True
             else:
@@ -43,4 +43,3 @@ class ModelDatabaseRouter(object):
                 return True
             else:
                 return False
-
