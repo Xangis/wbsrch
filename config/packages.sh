5a55481293d2be1784cbe700226720e48f8085de
xangis <xangis@epsilon>
2021-01-15 23:12:07 -0800

    Adjust packages.

--- config/packages.sh
+++ config/packages.sh
@@ -1,5 +1,5 @@
 sudo apt-get remove apache2
-sudo add-apt-repository ppa:certbot/certbot
+#sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
 sudo apt-get install certbot build-essential nginx python3-dev postgresql-client-12 postgresql-12 libpq-dev redis-server gettext node-uglify \
- python-virtualenv virtualenvwrapper libgeoip-dev dnsutils libpqxx-dev lm-sensors unzip usbmount uwsgi uwsgi-plugin-python zip exim4
+ libgeoip-dev dnsutils libpqxx-dev lm-sensors unzip usbmount zip exim4 python3-venv
