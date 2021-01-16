612f5ecb9fe2c1339dc63942e0e00f8a89229ccd
Xangis <jchampion@sigmacentauri.com>
2021-01-15 19:58:52 -0800

    Update packages to python3 / psql 12

--- config/packages.sh
+++ config/packages.sh
@@ -1,5 +1,5 @@
 sudo apt-get remove apache2
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
-sudo apt-get install certbot build-essential nginx python2.7-dev postgresql-client-10 postgresql-10 libpq-dev redis-server gettext node-uglify \
+sudo apt-get install certbot build-essential nginx python3-dev postgresql-client-12 postgresql-12 libpq-dev redis-server gettext node-uglify \
  python-virtualenv virtualenvwrapper libgeoip-dev dnsutils libpqxx-dev lm-sensors unzip usbmount uwsgi uwsgi-plugin-python zip exim4
