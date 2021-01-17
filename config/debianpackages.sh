c6e9bf152637b00797e71aa6073a95cfbc0c31d9
Jason Champion <jchampionWsigmacentauri.com>
2021-01-17 04:52:49 +0000

    Add command to install packages on a Debian system.

---
+++ config/debianpackages.sh
@@ -0,0 +1,1 @@
+sudo apt-get install certbot build-essential nginx python3-dev postgresql-client-11 postgresql-11 libpq-dev redis-server gettext node-uglify libgeoip-dev dnsutils libpqxx-dev unzip zip python3-venv
