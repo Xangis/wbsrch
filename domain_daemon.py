0822c081accb6083119f52e847150ba2583667e7
Jason Champion <jchampion@zetacentauri.com>
2021-01-16 20:16:01 -0800

    Change domain daemon to work in batches of 50.

--- domain_daemon.py
+++ domain_daemon.py
@@ -3,9 +3,9 @@ import time
 
 while True:
     # Update domains slowly to avoid annoying DNS servers. Do sequential, then random, then update old domains.
-    call(['python', 'manage.py', 'domain_update', '-m', '20', '-s', '30'])
+    call(['python', 'manage.py', 'domain_update', '-m', '50', '-s', '30'])
     time.sleep(5)
-    call(['python', 'manage.py', 'domain_update', '-m', '20', '-s', '30', '-r'])
+    call(['python', 'manage.py', 'domain_update', '-m', '50', '-s', '30', '-r'])
     time.sleep(5)
-    call(['python', 'manage.py', 'domain_update', '-m', '20', '-s', '30', '-x'])
+    call(['python', 'manage.py', 'domain_update', '-m', '50', '-s', '30', '-x'])
     time.sleep(5)
