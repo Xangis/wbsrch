972c5d76e483b02640b3ba94236639c1d9733832
xangis <xangis@epsilon>
2021-01-16 21:58:56 -0800

    Switch to argparse for arguments.

--- sync_crawls.py
+++ sync_crawls.py
@@ -1,10 +1,10 @@
 import psycopg2
-import optparse
+import argparse
 
-parser = optparse.OptionParser()
-parser.add_option('-i', '--input', action='store', type='string', dest='input', help='Input db, example: "dbname=suppliers user=postgres password=postgres"')
-parser.add_option('-o', '--output', action='store', type='string', dest='output', help='Output db, example: "dbname=zetaweb user=postgres password=postgres"')
-(options, args) = parser.parse_args()
+parser = argparse.ArgumentParser(description="Sync data between databases")
+parser.add_argument('input', action='store', type=str, help='Input db, example: "dbname=suppliers user=postgres password=postgres"')
+parser.add_argument('output', action='store', type=str, help='Output db, example: "dbname=zetaweb user=postgres password=postgres"')
+options = parser.parse_args()
 
 print('Input: ' + options.input)
 print('Output: ' + options.output)
@@ -18,11 +18,11 @@ outcur = outdb.cursor()
 incur.execute("SELECT version()")
 outcur.execute("SELECT version()")
 
-in_version = incur.fetchone()
-print('In Database Version: ' + in_version)
+in_version = incur.fetchone()[0]
+print('In Database Version: {0}'.format(in_version))
 
-out_version = incur.fetchone()
-print('Out Database Version: ' + out_version)
+out_version = outcur.fetchone()[0]
+print('Out Database Version: {0}'.format(out_version))
 
 incur.close()
 outcur.close()
