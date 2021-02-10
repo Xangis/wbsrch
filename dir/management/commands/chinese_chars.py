a6bcd1432b286c508e16dffddb6be8358705b6bb
Jason Champion <jchampion@zetacentauri.com>
2021-02-09 21:47:11 -0800

    Port some code to python3

--- dir/management/commands/chinese_chars.py
+++ dir/management/commands/chinese_chars.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
 from dir.utils import GetSiteInfoModelFromLanguage
+from dir.models import BlockedSite
 import re
 
 
@@ -12,7 +13,7 @@ class Command(BaseCommand):
         # parser.add_argument('-d', '--detailed', default=False, action='store_true', dest='verbose', help='Run in verbose mode.')
         # parser.add_argument('-p', '--pending', default=False, action='store_true', dest='pending', help='Get pending term list from database.')
         parser.add_argument('-l', '--language', default='en', action='store', dest='language', help='Language to use for domains (default=en). It is dumb to use not english because if a domain is in another table, it has been categorized and has a domain info.')
-        # parser.add_argument('-r', '--reindex', default=False, action='store_true', dest='reindex', help='Reindex existing least-recently-indexed terms.')
+        parser.add_argument('-b', '--blockit', default=False, action='store_true', dest='blockit', help='Block any site that meets the threshold.')
         parser.add_argument('-m', '--max', default=5, action='store', type=int, dest='max', help='Max number of domains to update. (default=5)')
         parser.add_argument('-s', '--sleep', default=15, action='store', type=int, dest='sleep', help='Time to sleep between domain queries. (default=15)')
         parser.add_argument('-o', '--offset', default=0, action='store', type=int, dest='offset', help='Domain slice offset - distance from beginning to start. (default=0)')
@@ -24,6 +25,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         site_model = GetSiteInfoModelFromLanguage(options['language'])
         max = options['max']
+        blockit = options.get('blockit', False)
+        print('Blockit is {0}'.format(blockit))
         offset = options['offset']
         threshold = options['threshold']
         pages = site_model.objects.all().values('pagetitle', 'pagefirstheadtag', 'pagefirsth2tag', 'pagefirsth3tag', 'id', 'rooturl')[offset:offset + max]
@@ -75,9 +78,16 @@ class Command(BaseCommand):
                         domains[rooturl] = 1
                     break
             processed = processed + 1
-        results = sorted(domains.iteritems(), key=lambda item: item[1], reverse=False)
+        results = sorted(domains.items(), key=lambda item: item[1], reverse=False)
         for result in results:
             if result[1] >= threshold:
                 print('{0} - {1}'.format(result[1], result[0]))
                 printed = printed + 1
+                if blockit:
+                    print('Blocking {0}'.format(result[0]))
+                    bsite = BlockedSite()
+                    bsite.url = result[0]
+                    bsite.reason = 8
+                    bsite.save()
+
         print('Processed {0} urls and found {1} domains that are probably asian and {2} met print threshold.'.format(processed, len(results), printed))
