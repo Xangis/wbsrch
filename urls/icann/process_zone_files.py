c1721b00db8f6eb0c82e3500aa5ab0653e94149b
Jason Champion <jchampion@zetacentauri.com>
2021-02-14 15:26:45 -0800

    Skip mail servers and nameservers when building a domain list from zone files because they're useless.

--- urls/icann/process_zone_files.py
+++ urls/icann/process_zone_files.py
@@ -13,6 +13,13 @@ parser.add_argument('directory', action='store', type=str, help='Input directory
 options = parser.parse_args()
 
 
+nameserver_list = ['ns1', 'ns2', 'ns3', 'ns4', 'ns5', 'ns6', 'ns7', 'ns8', 'ns9', 'ns01', 'ns02', 'ns03', 'ns04', 'ns05', 'ns06', 'ns07', 'ns08', 'ns09',
+                   'ns10', 'ns11', 'ns12', 'ns13', 'ns14', 'ns15', 'ns16', 'ns17', 'ns18', 'ns19',
+                   'ns20', 'ns21', 'ns22', 'ns23', 'ns24', 'ns25', 'ns26', 'ns27', 'ns28', 'ns29',
+                   'ns30', 'ns31', 'ns32', 'ns33', 'ns34', 'ns35', 'ns36', 'ns37', 'ns38', 'ns39',
+                   'ns40', 'ns41', 'ns42', 'ns43', 'ns44', 'ns45', 'ns46', 'ns47', 'ns48', 'ns49',
+                   'ns50', 'ns51', 'ns52', 'ns53', 'ns54', 'ns55', 'ns56', 'ns57', 'ns58', 'ns59']
+
 def ProcessDomains(filename):
     processed = 0
     domains = set()
@@ -26,6 +33,17 @@ def ProcessDomains(filename):
         # Skip the domain root.
         if '.' not in domain:
             continue
+        if domain.startswith('ns.'):
+            continue
+        if domain.startswith('mail.'):
+            pieces = domain.split('.')
+            if len(pieces) > 2:
+                continue
+        if domain.startswith('ns'):
+            pieces = domain.split('.')
+            if len(pieces) > 2:
+                if pieces[0] in nameserver_list:
+                    continue
         if domain not in domains:
             #print('Domain: {0}'.format(domain))
             domains.add(domain)
