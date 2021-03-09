c5a5976f7d20eb717b0528973c8e30f2038e5166
Jason Champion <jchampion@zetacentauri.com>
2021-03-09 22:07:41 +0000

    update month

--- sql/adultdomains.sh
+++ sql/adultdomains.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-time psql -c "COPY ( select url from dir_blockedsite where reason = 4 ) TO STDOUT WITH CSV HEADER " indexes > /tmp/adult_domains_2021-02.csv
+time psql -c "COPY ( select url from dir_blockedsite where reason = 4 ) TO STDOUT WITH CSV HEADER " indexes > /tmp/adult_domains_2021-03.csv
