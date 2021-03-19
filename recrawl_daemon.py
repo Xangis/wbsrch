ec5845a6445413a99e516eea038a475f6397af12
Jason Champion <jchampion@zetacentauri.com>
2021-03-19 19:44:51 +0000

    Parameterize sleep time in recrawl daemon.

--- recrawl_daemon.py
+++ recrawl_daemon.py
@@ -1,5 +1,11 @@
 from subprocess import call
+import optparse
+
+parser = optparse.OptionParser()
+parser.set_defaults(seconds=5)
+parser.add_option('-s', '--seconds', action='store', default=5, type='int', dest='seconds', help='Seconds between pages. (default=5)')
+(options, args) = parser.parse_args()
 
 while True:
     for x in range(100000, -20, -20):
-        call(['python', 'manage.py', 'crawl', '-r', '-m', '20', '-s', '5', '-o', str(x)])
+        call(['python', 'manage.py', 'crawl', '-r', '-m', '20', '-s', str(options.seconds), '-o', str(x)])
