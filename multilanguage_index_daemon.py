3585eb39cfbda08ec36c365eb30dc6fec783a97a
Jason Champion <jchampion@zetacentauri.com>
2021-03-30 05:11:06 +0000

    Add new languages to multilanguage crawl and index daemons.

--- multilanguage_index_daemon.py
+++ multilanguage_index_daemon.py
@@ -7,10 +7,23 @@ parser.set_defaults(seconds=1)
 parser.add_option('-s', '--seconds', action='store', default=1, type='int', dest='seconds', help='Seconds between indexes. (default=1)')
 (options, args) = parser.parse_args()
 
-language_list = ['en', 'an', 'ca', 'cs', 'cy', 'da', 'de', 'el', 'es', 'et', 'eu', 'fi', 'fr', 'gl', 'ha', 'hr', 'hu', 'is', 'it', 'lt', 'lv', 'nl', 'no', 'pl', 'pt', 'ro', 'rw', 'sl', 'sn', 'so', 'sv', 'sw', 'tr', 'wo', 'xh', 'yo', 'zu']
+language_list = ['en', 'af', 'an', 'bs', 'ca', 'cs', 'cy', 'da', 'de', 'el', 'eo', 'es', 'et', 'eu', 'fi', 'fo', 'fr', 'ga', 'gl', 'ha', 'hr', 'hu', 'is', 'it', 'la', 'lb', 'lt', 'lv', 'mg', 'mt', 'nl', 'no', 'oc', 'pl', 'pt', 'qu', 'ro', 'rw', 'sk', 'sl', 'sn', 'so', 'sv', 'sw', 'tr', 'vo', 'wa', 'wo', 'xh', 'yo', 'zu']
 
 # Index ratio controls how quickly we cycle through terms for a language.
 # Counts for languages not in the index are ignored.
+lang_index_counts = {
+  'af': 2, 'an': 2, 'bs': 2, 'ca': 3, 'cs': 8,
+  'cy': 2, 'da': 8, 'de': 30, 'el': 6, 'eo': 2,
+  'es': 25, 'et': 4, 'eu': 2, 'fi': 6, 'fo': 2,
+  'fr': 20, 'ga': 2, 'gl': 2, 'ha': 2, 'hr': 4,
+  'hu': 4, 'is': 2, 'it': 15, 'la': 2, 'lb': 2,
+  'lt': 3, 'lv': 2, 'mg': 2, 'mt': 2, 'nl': 15,
+  'no': 5, 'oc': 2, 'pl': 8, 'pt': 12, 'qu': 2,
+  'ro': 3, 'rw': 2, 'sk': 3, 'sl': 3, 'sn': 1,
+  'so': 1, 'sv': 7, 'sw': 2, 'tr': 6, 'vo': 2,
+  'wa': 2, 'wo': 1, 'xh': 2, 'yo': 1, 'zu': 1
+}
+
 lang_index_counts = {'lt': 4, 'lv': 3, 'is': 4, 'sw': 2, 'yo': 2, 'so': 2, 'wo': 2, 'ha': 2, 'rw': 2, 'sn': 2, 'ca': 2, 'cs': 15, 'fi': 10, 'el': 10, 'hu': 8, 'pl': 10, 'tr': 14, 'da': 7, 'et': 4, 'de': 15, 'no': 5, 'pt': 10, 'nl': 8, 'sk': 8, 'sl': 8, 'es': 12, 'sv': 8, 'hr': 5, 'it': 8, 'fr': 12, 'ro': 6}
 
 # Slowly indexes terms, one per minute maximum.
--- multilanguage_recrawl_daemon.py
+++ multilanguage_recrawl_daemon.py
@@ -6,14 +6,28 @@ parser.set_defaults(seconds=5)
 parser.add_option('-s', '--seconds', action='store', default=5, type='int', dest='seconds', help='Seconds between pages. (default=5)')
 (options, args) = parser.parse_args()
 
-language_list = ['en', 'an', 'ca', 'cs', 'cy', 'da', 'de', 'el', 'es', 'et', 'eu', 'fi', 'fr', 'gl', 'ha', 'hr', 'hu', 'is', 'it', 'lt', 'lv', 'nl', 'no', 'pl', 'pt', 'ro', 'rw', 'sl', 'sn', 'so', 'sv', 'sw', 'tr', 'wo', 'xh', 'yo', 'zu']
+language_list = ['en', 'af', 'an', 'bs', 'ca', 'cs', 'cy', 'da', 'de', 'el',
+                 'eo', 'es', 'et', 'eu', 'fi', 'fo', 'fr', 'ga', 'gl', 'ha',
+                 'hr', 'hu', 'is', 'it', 'la', 'lb', 'lt', 'lv', 'mg', 'mt',
+                 'nl', 'no', 'oc', 'pl', 'pt', 'qu', 'ro', 'rw', 'sk', 'sl',
+                 'sn', 'so', 'sv', 'sw', 'tr', 'vo', 'wa', 'wo', 'xh', 'yo',
+                 'zu']
 
 
 # Based on a ratio vs French at 100,000 URLs wanting to recrawl 20 per round.
 # Adjust accordingly as indexes evolve.
 # Default is 1, which is appropriate for all small (<5k) indexes.
 lang_crawl_counts = {
-  'cs': 3, 'fr': 20, 'nl': 10, 'de': 50, 'hu': 2, 'it': 15, 'pl': 8, 'pt': 3, 'ro': 2, 'es': 14, 'sv': 7, 'tr': 5
+  'af': 2, 'an': 2, 'bs': 2, 'ca': 3, 'cs': 8,
+  'cy': 2, 'da': 8, 'de': 50, 'el': 6, 'eo': 2,
+  'es': 25, 'et': 4, 'eu': 2, 'fi': 6, 'fo': 2,
+  'fr': 20, 'ga': 2, 'gl': 2, 'ha': 2, 'hr': 4,
+  'hu': 4, 'is': 2, 'it': 15, 'la': 2, 'lb': 2,
+  'lt': 3, 'lv': 2, 'mg': 2, 'mt': 2, 'nl': 15,
+  'no': 5, 'oc': 2, 'pl': 8, 'pt': 12, 'qu': 2,
+  'ro': 3, 'rw': 2, 'sk': 3, 'sl': 3, 'sn': 1,
+  'so': 1, 'sv': 7, 'sw': 2, 'tr': 6, 'vo': 2,
+  'wa': 2, 'wo': 1, 'xh': 2, 'yo': 1, 'zu': 1
 }
 
 while True:
@@ -21,5 +35,5 @@ while True:
         for language in language_list:
             if language == 'en':
                 continue
-            lang_crawl_count = lang_crawl_counts.get(language, 1)
+            lang_crawl_count = lang_crawl_counts.get(language, 10)
             call(['python', 'manage.py', 'crawl', '-r', '-m', str(lang_crawl_count), '-s', str(options.seconds), '-o', str(x * lang_crawl_count), '-l', language])
