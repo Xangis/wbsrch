e49a187bf0b84f09d58ce10ced86248b2fca0b90
Jason Champion <jchampion@zetacentauri.com>
2021-05-01 20:57:03 +0000

    Change crawl ratios based on number of pages in database.

--- multilanguage_recrawl_daemon.py
+++ multilanguage_recrawl_daemon.py
@@ -3,6 +3,7 @@ import optparse
 
 parser = optparse.OptionParser()
 parser.set_defaults(seconds=5)
+parser.add_option('-o', '--offset', action='store', default=0, type='int', dest='offset', help='Offset multiplier, 1 = skip one batch of urls, i.e. 1x20x100 for fr (default=0)')
 parser.add_option('-s', '--seconds', action='store', default=5, type='int', dest='seconds', help='Seconds between pages. (default=5)')
 (options, args) = parser.parse_args()
 
@@ -18,16 +19,16 @@ language_list = ['en', 'af', 'an', 'bs', 'ca', 'cs', 'cy', 'da', 'de', 'el',
 # Adjust accordingly as indexes evolve.
 # Default is 1, which is appropriate for all small (<5k) indexes.
 lang_crawl_counts = {
-  'af': 2, 'an': 2, 'bs': 2, 'ca': 3, 'cs': 8,
-  'cy': 2, 'da': 8, 'de': 50, 'el': 6, 'eo': 2,
-  'es': 25, 'et': 4, 'eu': 2, 'fi': 6, 'fo': 2,
-  'fr': 20, 'ga': 2, 'gl': 2, 'ha': 2, 'hr': 4,
-  'hu': 4, 'is': 2, 'it': 15, 'la': 2, 'lb': 2,
-  'lt': 3, 'lv': 2, 'mg': 2, 'mt': 2, 'nl': 15,
-  'no': 5, 'oc': 2, 'pl': 8, 'pt': 12, 'qu': 2,
-  'ro': 3, 'rw': 2, 'sk': 3, 'sl': 3, 'sn': 1,
-  'so': 1, 'sv': 7, 'sw': 2, 'tr': 6, 'vo': 2,
-  'wa': 2, 'wo': 1, 'xh': 2, 'yo': 1, 'zu': 1
+  'af': 1, 'an': 1, 'bs': 1, 'ca': 3, 'cs': 8,
+  'cy': 1, 'da': 8, 'de': 50, 'el': 6, 'eo': 1,
+  'es': 25, 'et': 2, 'eu': 1, 'fi': 7, 'fo': 1,
+  'fr': 20, 'ga': 1, 'gl': 1, 'ha': 1, 'hr': 4,
+  'hu': 8, 'is': 1, 'it': 15, 'la': 1, 'lb': 2,
+  'lt': 3, 'lv': 2, 'mg': 1, 'mt': 1, 'nl': 15,
+  'no': 6, 'oc': 1, 'pl': 12, 'pt': 18, 'qu': 1,
+  'ro': 5, 'rw': 2, 'sk': 4, 'sl': 2, 'sn': 1,
+  'so': 1, 'sv': 7, 'sw': 1, 'tr': 8, 'vo': 1,
+  'wa': 1, 'wo': 1, 'xh': 1, 'yo': 1, 'zu': 1
 }
 
 while True:
@@ -36,4 +37,5 @@ while True:
             if language == 'en':
                 continue
             lang_crawl_count = lang_crawl_counts.get(language, 10)
-            call(['python', 'manage.py', 'crawl', '-r', '-m', str(lang_crawl_count), '-s', str(options.seconds), '-o', str(x * lang_crawl_count), '-l', language])
+            offset = x * lang_crawl_count + (100 * lang_crawl_count * options.offset)
+            call(['python', 'manage.py', 'crawl', '-r', '-m', str(lang_crawl_count), '-s', str(options.seconds), '-o', str(offset), '-l', language])
