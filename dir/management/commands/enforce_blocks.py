afcae3627b10d9d402a4fefd9c99b5a9e3f88e0e
Jason Champion <jchampion@zetacentauri.com>
2021-05-04 22:02:52 +0000

    Add offset parameter to enforce_blocks admin command.

--- dir/management/commands/enforce_blocks.py
+++ dir/management/commands/enforce_blocks.py
@@ -14,10 +14,12 @@ class Command(BaseCommand):
     """
 
     def add_arguments(self, parser):
-        parser.add_argument('-a', '--alll', default=False, action='store_true', dest='all', help='Check all language indexes for blocked site pages (default=False).')
+        parser.add_argument('-a', '--all', default=False, action='store_true', dest='all', help='Check all language indexes for blocked site pages (default=False).')
+        parser.add_argument('-o', '--offset', default=0, action='store', type=int, dest='offset', help='Record offset for check.')
 
     def handle(self, *args, **options):
-        sites = BlockedSite.objects.all().order_by('url')
+        offset = options['offset']
+        sites = BlockedSite.objects.all().order_by('url')[offset:]
         count = 0
         for site in sites:
             RemoveURLsForDomain(site.url, verbose=True, all_languages=options['all'])
