861624379cd99246ca9ec1184a83fd1f3dd12124
Jason Champion <jchampion@zetacentauri.com>
2021-06-05 19:25:33 +0000

    Allow block_site to read a list of domains from a file.

--- dir/management/commands/block_site.py
+++ dir/management/commands/block_site.py
@@ -3,6 +3,7 @@ from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
 from dir.models import BlockedSite
 from dir.utils import RemoveURLsForDomain
+import codecs
 
 
 def TrimDomain(domain):
@@ -27,21 +28,39 @@ class Command(BaseCommand):
     """
 
     def add_arguments(self, parser):
-        parser.add_argument('-s', '--site', action='store', dest='site', help='Site to block.'),
-        parser.add_argument('-r', '--reason', default=8, action='store', type=int, dest='reason', help='Reason (4=porn, 5=pills, 8=unindexed language, 20=arabic, 21=chinese, 25=japanese, 28=russian, 80=jerks)'),
+        parser.add_argument('-s', '--site', default=None, action='store', dest='site', help='Site to block.'),
+        parser.add_argument('-r', '--reason', default=None, action='store', type=int, dest='reason', help='Reason (4=porn, 5=pills, 8=unindexed language, 20=arabic, 21=chinese, 25=japanese, 28=russian, 80=jerks)'),
+        parser.add_argument('-f', '--file', default=None, action='store', dest='file', help='Load list of sites to block from specified file.')
 
     def handle(self, *args, **options):
         site = options.get('site')
-        site = TrimDomain(site)
         reason = int(options.get('reason'))
-        print('Blocking {0} for reason {1}'.format(site, reason))
-        try:
-            BlockedSite.objects.get(url=site)
-            # If the domain is already blocked, the URL must have been added erroneously.
-            # in that case, just delete it.
-            RemoveURLsForDomain(site)
-        except ObjectDoesNotExist:
-            bsite = BlockedSite()
-            bsite.url = site
-            bsite.reason = reason
-            bsite.save()
+        filename = options.get('file', None)
+
+        sites = set()
+        if site:
+            site = TrimDomain(site)
+            sites.add(site)
+        elif filename:
+            f = open(filename, 'rb')
+            reader = codecs.getreader('utf8')(f)
+            for line in reader:
+                trimmed = TrimDomain(line)
+                if trimmed:
+                    sites.add(trimmed)
+        else:
+            print('Either site or filename must be specified.')
+            return
+
+        for site in sites:
+            print('Blocking {0} for reason {1}'.format(site, reason))
+            try:
+                BlockedSite.objects.get(url=site)
+                # If the domain is already blocked, the URL must have been added erroneously.
+                # in that case, just delete it.
+                RemoveURLsForDomain(site)
+            except ObjectDoesNotExist:
+                bsite = BlockedSite()
+                bsite.url = site
+                bsite.reason = reason
+                bsite.save()
